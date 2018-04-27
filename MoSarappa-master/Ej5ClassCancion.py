ead84539ae31f2494313f399cdf25596f3060a9f
Lucas <31509642+Lsarappa@users.noreply.github.com>
2018-04-26 23:33:29 -0300

    Add files via upload

---
+++ MoSarappa-master/Ej5ClassCancion.py
@@ -0,0 +1,29 @@
+from Ej5ClassPersonas import Artistas
+from Ej5ClassPersonas import Autores
+
+class Cancion(object):
+
+    Titulo = None
+
+    def __init__(self, Titulo):
+
+        self.Titulo = Titulo
+
+        self.Lista_Artistas = []
+        self.Lista_Autores = []
+
+    def AgregarArtista (self, unArtista):
+
+        for item in self.Lista_Artistas:
+            if item == unArtista:
+                return False
+
+        self.Lista_Artistas.append(unArtista)
+
+    def AgregarAutor (self, unAutor):
+
+        for item in self.Lista_Autores:
+            if item == unAutor:
+                return False
+
+        self.Lista_Autores.append(unAutor)
